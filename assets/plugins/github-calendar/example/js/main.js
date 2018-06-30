21c61e3753c75e7110010c808e24a52b32b2ff5c
leoallvez <leoallvez@hotmail.com>
2018-06-29 23:46:06 -0300

    Implementação do plugin javascript github-calendar em português do Brasil

--- assets/plugins/github-calendar/example/js/main.js
+++ assets/plugins/github-calendar/example/js/main.js
@@ -1,1 +1,6 @@
-GitHubCalendar(".calendar", "IonicaBizau");
+GitHubCalendar(".calendar", "leoallvez");
+
+$(document).ready(function () {
+    //$(".calendar > .f4").text("Rapaz!");
+    console.log("ready!");
+});
