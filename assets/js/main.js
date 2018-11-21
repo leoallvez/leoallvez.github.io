80111eb9ae8ef5e12ba6be23753c60f7e399124c
Leonardo Alves <leoallvez@hotmail.com>
2018-11-20 23:37:28 -0300

    Refatorando

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function($) {
     /* Bootstrap Tooltip for Skillset */
     $('.level-label').tooltip();
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    GitHubCalendar(".calendar", "leoallvez");
+    //GitHubCalendar(".calendar", "leoallvez");
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
     GitHubActivity.feed({ username: "leoallvez", selector: "#ghfeed" });
 });
